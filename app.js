6be051da74c32fd9aa78bfb494919d700801051a
roberteftene <roberteftene25@gmail.com>
2020-10-23 15:52:46 +0300

    feature- adding, deleting todo from local storage

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
+const filterOption = document.querySelector('.filter-todo'); 
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click',filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -23,6 +25,9 @@ function addTodo(event) {
     newTodo.classList.add('todo-item');
 
     todoDiv.appendChild(newTodo);
+
+    // Add todo to local storage
+    saveLocalTodos(todoInput.value);
 
     //Create checked mark button
     const completedButton = document.createElement('button');
@@ -52,7 +57,12 @@ function deleteCheck(event) {
     //Delete todo
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add("fall")
+        removeLocalTodos(todo);
+        todo.addEventListener('transitionend', () => {
+            todo.remove();
+        })
     }
 
     //Checkmark
@@ -60,4 +70,99 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach((todo) => {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
+}
+
+
+function saveLocalTodos(todo) {
+
+    //Check if there are any todos
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+}
+
+function getTodos() {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach((todo) =>  {
+    //Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todo;
+    newTodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newTodo);
+
+    //Create checked mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+
+    todoDiv.appendChild(completedButton);
+
+    //Create trash mark button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+
+    todoDiv.appendChild(trashButton);
+
+    //Append to list
+    todoList.appendChild(todoDiv);
+    })
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex),1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    
 }
